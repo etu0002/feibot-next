38526eb5f34e5ab8652fa838d4aa6f767209ef06
2B <hifisaputra1@gmail.com>
2022-10-30 06:58:52 +0800

    fix on message event

---
+++ src/commands/pint.ts
@@ -0,0 +1,8 @@
+import { SlashCommandBuilder, Interaction } from 'discord.js'
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+    async execute(interaction: any) {
+        await interaction.reply('Pong!')
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import { Client, GatewayIntentBits } from 'discord.js'
+import 'module-alias/register'
+import { Client, GatewayIntentBits, Message, Events } from 'discord.js'
 import config from '@src/config'
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
 })
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return
+client.once(Events.ClientReady, (client: Client) => {
+    console.log(`Ready! Logged in as ${client.user.tag}`)
+})
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!')
-    }
+client.on(Events.MessageCreate, (message: Message) => {
+    console.log('on message', message.cleanContent, message.channelId)
 })
 
 client.login(config.discord.token)
