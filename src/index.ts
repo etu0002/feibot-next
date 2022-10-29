f778f50efa5e0286657fdf685b05635e31ac33ff
2B <hifisaputra1@gmail.com>
2022-10-30 04:41:05 +0800

    added eslint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-import { REST } from 'discord.js'
+import { Client, GatewayIntentBits } from 'discord.js'
+import config from '@src/config'
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!')
+    }
+})
+
+client.login(config.discord.token)
