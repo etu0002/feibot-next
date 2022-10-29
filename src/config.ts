3246d0373e83db976152fcbcf202e2f8367c805f
2B <hifisaputra1@gmail.com>
2022-10-30 04:26:36 +0800

    added config for database user and password

--- src/config.ts
+++ src/config.ts
@@ -4,21 +4,27 @@ dotenv.config()
 
 export default {
     /**
-     * @description MongoDB database connection config
+     * @description MongoDB database connection
+     * @see https://www.mongodb.com if you are not planning on hosting your own database server
      *
      * @type {object}
      * @property {string} host MongoBD host
      * @property {number} port MongoDB post
      * @property {string} name MongoDB database name
+     * @property {string} user MongoDB database user, required if your database has auth enabled
+     * @property {string} password MongoDB database password, same with user required if auth enabled
      */
     database: {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 27017,
-        name: process.env.DB_NAME || '0'
+        name: process.env.DB_NAME || '0',
+        user: process.env.DB_USER || '',
+        password: process.env.DB_PASSWORD || ''
     },
 
     /**
-     * @description Discord configuration
+     * @description Discord Bot API configuration
+     * @see https://discord.com/developers/docs/getting-started#creating-an-app
      *
      * @type {object}
      * @property {string} token discord bot token
@@ -28,11 +34,11 @@ export default {
     },
 
     /**
-     * @description Bot configuration
+     * @description Your bot name and identity
      *
      * @type {object}
      * @property {string} name Bot name
-     * @property {string} identity Bot default identity
+     * @property {string} identity Bot default identity, will be used if custom identity has not been set
      */
     bot: {
         name: process.env.BOT_NAME || 'Fei',
@@ -41,6 +47,7 @@ export default {
 
     /**
      * @description OpenAI API configuration
+     * @see https://openai.com/api/
      *
      * @type {object}
      * @property {string} apiKey OpenAI API key
